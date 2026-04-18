19e1db4feaf84937f65837a480c2cf809bd4964b
habacuc.eth <habacuc.vera@gmail.com>
2026-04-17 18:13:49 -0600

    docs/members logos and icons

--- Site/tailwind.config.mjs
+++ Site/tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}'],
+  darkMode: ['selector', '[data-theme="dark"]'],
   theme: {
     extend: {
       colors: {
